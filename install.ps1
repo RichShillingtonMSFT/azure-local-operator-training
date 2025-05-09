a6a087f1f08727bc8627dc4ec4a6877d1f635ae8
Rich Shillington <Rich.Shillington@microsoft.com>
2025-05-09 17:55:29 -0400

    Commit

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 
 az login
-az group create --name "Student01-Rg" --location "eastus"
+az group create --name "Student01-Rg" --location "eastus2"
 az deployment group create -g "Student01-Rg" -f "./bicep/main.bicep" -p "./bicep/main.bicepparam"
