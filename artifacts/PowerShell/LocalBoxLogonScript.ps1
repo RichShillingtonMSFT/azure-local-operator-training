e8c79616ecf0c04a0648534efdade94c42291f36
Rich Shillington <rich.shillington@microsoft.com>
2025-05-14 11:30:06 -0400

    Commit

--- artifacts/PowerShell/LocalBoxLogonScript.ps1
+++ artifacts/PowerShell/LocalBoxLogonScript.ps1
@@ -148,6 +148,44 @@ Unregister-ScheduledTask -TaskName "LocalBoxLogonScript" -Confirm:$false
 Write-Header "Changing wallpaper"
 
 # bmp file is required for BGInfo
+function Convert-JSImageToBitMap {
+    param (
+        $SourceFilePath,
+        $DestinationFilePath
+    )
+    [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+    $file = Get-Item $SourceFilePath
+    $convertfile = new-object System.Drawing.Bitmap($file.Fullname)
+    $convertfile.Save($DestinationFilePath, "bmp")
+}
+
+function Set-JSDesktopBackground {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$ImagePath
+    )
+
+$code = @'
+    using System.Runtime.InteropServices;
+    namespace Win32{
+
+        public class Wallpaper{
+            [DllImport("user32.dll", CharSet=CharSet.Auto)]
+            static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+
+            public static void SetWallpaper(string thePath){
+                SystemParametersInfo(20,0,thePath,3);
+            }
+        }
+    }
+'@
+
+Add-Type $code
+[Win32.Wallpaper]::SetWallpaper($ImagePath)
+
+}
+
 Convert-JSImageToBitMap -SourceFilePath "$Env:LocalBoxDir\wallpaper.png" -DestinationFilePath "$Env:LocalBoxDir\wallpaper.bmp"
 
 Set-JSDesktopBackground -ImagePath "$Env:LocalBoxDir\wallpaper.bmp"
